4ce1772d5c84e8547e05dc343de45059da760420
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2019-03-22 02:36:18 -0300

    Modified worker to produce time lagged columns for time series

--- worker.py
+++ worker.py
@@ -148,7 +148,7 @@ try:
             crimes_ts_pt = crimes_ts[((crimes_ts['Community Area'] == comm) & (crimes_ts['Primary Type'] == cts))]
             crimes_ts_pt.drop(columns=['Date','Primary Type','Community Area'], inplace=True)
             columns = [crimes_ts_pt.shift(i) for i in range(1, lag+1)]
-	        columns.append(crimes_ts_pt)
+            columns.append(crimes_ts_pt)
 	        crimes_ts_pt = pd.concat(columns, axis=1)
             crimes_ts_pt = crimes_ts_pt[lag:]
             partials.append(crimes_ts_pt)
