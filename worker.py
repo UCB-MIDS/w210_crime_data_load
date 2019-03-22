27765d913c3d61fd291a62229f902ae4a3651540
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2019-03-22 12:31:04 -0300

    Small fix to worker.py on time series columns

--- worker.py
+++ worker.py
@@ -150,7 +150,7 @@ try:
             crimes_ts_pt = crimes_ts[((crimes_ts['Community Area'] == comm) & (crimes_ts['Primary Type'] == ct))]
             crimes_ts_pt.drop(columns=['Date','Primary Type','Community Area'], inplace=True)
             columns = [crimes_ts_pt.shift(i) for i in range(1, lag+1)]
-            columns.append(crimes_ts_pt)
+            columns.append(crimes_ts_pt.iloc[:,-1:])
             crimes_ts_pt = pd.concat(columns, axis=1)
             crimes_ts_pt = crimes_ts_pt[lag:]
             partials_train.append(crimes_ts_pt[val_size+1:])
